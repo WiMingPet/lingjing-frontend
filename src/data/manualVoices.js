f3ff2269ad3835380b315b69c84f53cbfe798bc0
sumaai <sumaai@sumaai.cn>
2026-05-03 21:19:08 +0800

    新增5个自定义音色配置文件

---
+++ src/data/manualVoices.js
@@ -0,0 +1,32 @@
+export const MANUAL_VOICES = [
+  {
+    "id": "879902182131798065",
+    "name": "知性学姐",
+    "preview_url": "https://v2-kling.kechuangai.com/bs2/upload-ylab-stunt/muse/867844650139664460/AUDIO/20260503/58bba308094725230dcccf9e6d21216d-8e3de7e3-705b-4f60-b9dd-b0e261d703fe.quality.wav?x-kcdn-pid=112452",
+    "type": "clone"
+  },
+  {
+    "id": "879902230194180168",
+    "name": "温雅老师",
+    "preview_url": "https://v2-kling.kechuangai.com/bs2/upload-ylab-stunt/muse/867844650139664460/AUDIO/20260503/b83166509060089d05e25ed4b52a8336-a8d5b6a8-517a-450f-a4d7-4f85a220c011.quality.wav?x-kcdn-pid=112452",
+    "type": "clone"
+  },
+  {
+    "id": "879902281675067434",
+    "name": "活力男声",
+    "preview_url": "https://v2-kling.kechuangai.com/bs2/upload-ylab-stunt/muse/867844650139664460/AUDIO/20260503/4e89bfaf20a4d94611601e04bb0d3b76-d77db66c-e3db-4196-8a13-e51d34fcc5cf.quality.wav?x-kcdn-pid=112452",
+    "type": "clone"
+  },
+  {
+    "id": "879902330958274592",
+    "name": "磁性大叔",
+    "preview_url": "https://v2-kling.kechuangai.com/bs2/upload-ylab-stunt/muse/867844650139664460/AUDIO/20260503/61d8258cb67263fdd056028db0f04bb6-1949b488-cca5-4384-86c5-5a7dab23224a.quality.wav?x-kcdn-pid=112452",
+    "type": "clone"
+  },
+  {
+    "id": "879902381856170004",
+    "name": "温柔解说",
+    "preview_url": "https://v2-kling.kechuangai.com/bs2/upload-ylab-stunt/muse/867844650139664460/AUDIO/20260503/8ba80fa0187b1900f671f19926dae947-63ccce26-b33c-4e68-83d0-c84b040221d2.quality.wav?x-kcdn-pid=112452",
+    "type": "clone"
+  }
+];
