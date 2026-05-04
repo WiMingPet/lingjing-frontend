d573ab926367c2f1d92ec29765173369310d7de2
sumaai <sumaai@sumaai.cn>
2026-05-04 09:41:42 +0800

    新增5个音色：小静、小芊、智薇、智萱、智希

--- src/data/manualVoices.js
+++ src/data/manualVoices.js
@@ -1,32 +1,62 @@
 export const MANUAL_VOICES = [
   {
-    "id": "879902182131798065",
-    "name": "知性学姐（独）",
-    "preview_url": "https://v2-kling.kechuangai.com/bs2/upload-ylab-stunt/muse/867844650139664460/AUDIO/20260503/58bba308094725230dcccf9e6d21216d-8e3de7e3-705b-4f60-b9dd-b0e261d703fe.quality.wav?x-kcdn-pid=112452",
+    "id": "880091449093152858",
+    "name": "知性学姐(独）",
+    "preview_url": "https://v2-kling.kechuangai.com/bs2/upload-ylab-stunt/muse/867844650139664460/AUDIO/20260504/58bba308094725230dcccf9e6d21216d-69695ff5-2fcb-4770-9370-454da3536290.quality.wav?x-kcdn-pid=112452",
     "type": "clone"
   },
   {
-    "id": "879902230194180168",
+    "id": "880091502654263336",
     "name": "温雅老师（独）",
-    "preview_url": "https://v2-kling.kechuangai.com/bs2/upload-ylab-stunt/muse/867844650139664460/AUDIO/20260503/b83166509060089d05e25ed4b52a8336-a8d5b6a8-517a-450f-a4d7-4f85a220c011.quality.wav?x-kcdn-pid=112452",
+    "preview_url": "https://v2-kling.kechuangai.com/bs2/upload-ylab-stunt/muse/867844650139664460/AUDIO/20260504/b83166509060089d05e25ed4b52a8336-7ac20f98-d930-4190-9c64-a0054aeb50ae.quality.wav?x-kcdn-pid=112452",
     "type": "clone"
   },
   {
-    "id": "879902281675067434",
-    "name": "阳光男声",
-    "preview_url": "https://v2-kling.kechuangai.com/bs2/upload-ylab-stunt/muse/867844650139664460/AUDIO/20260503/4e89bfaf20a4d94611601e04bb0d3b76-d77db66c-e3db-4196-8a13-e51d34fcc5cf.quality.wav?x-kcdn-pid=112452",
+    "id": "880091548670124045",
+    "name": "活力男声",
+    "preview_url": "https://v2-kling.kechuangai.com/bs2/upload-ylab-stunt/muse/867844650139664460/AUDIO/20260504/4e89bfaf20a4d94611601e04bb0d3b76-df51e3df-b75c-4b2b-abf0-01336cf196ce.quality.wav?x-kcdn-pid=112452",
     "type": "clone"
   },
   {
-    "id": "879902330958274592",
-    "name": "沉稳大叔",
-    "preview_url": "https://v2-kling.kechuangai.com/bs2/upload-ylab-stunt/muse/867844650139664460/AUDIO/20260503/61d8258cb67263fdd056028db0f04bb6-1949b488-cca5-4384-86c5-5a7dab23224a.quality.wav?x-kcdn-pid=112452",
+    "id": "880091602185248775",
+    "name": "磁性大叔",
+    "preview_url": "https://v2-kling.kechuangai.com/bs2/upload-ylab-stunt/muse/867844650139664460/AUDIO/20260504/61d8258cb67263fdd056028db0f04bb6-4dd0f088-5040-4bf8-a280-09daed92dfb0.quality.wav?x-kcdn-pid=112452",
     "type": "clone"
   },
   {
-    "id": "879902381856170004",
-    "name": "情感解说",
-    "preview_url": "https://v2-kling.kechuangai.com/bs2/upload-ylab-stunt/muse/867844650139664460/AUDIO/20260503/8ba80fa0187b1900f671f19926dae947-63ccce26-b33c-4e68-83d0-c84b040221d2.quality.wav?x-kcdn-pid=112452",
+    "id": "880091651543683127",
+    "name": "温柔解说",
+    "preview_url": "https://v2-kling.kechuangai.com/bs2/upload-ylab-stunt/muse/867844650139664460/AUDIO/20260504/8ba80fa0187b1900f671f19926dae947-37424eb7-5863-45eb-9f7e-1ab52cd6452d.quality.wav?x-kcdn-pid=112452",
+    "type": "clone"
+  },
+  {
+    "id": "880091702546415696",
+    "name": "小静",
+    "preview_url": "https://v2-kling.kechuangai.com/bs2/upload-ylab-stunt/muse/867844650139664460/AUDIO/20260504/1160cd627fc3ff6f7401e16632b26d68-5bf5b8b7-6ae7-4268-9070-854debfc8e3b.quality.wav?x-kcdn-pid=112452",
+    "type": "clone"
+  },
+  {
+    "id": "880091749082214494",
+    "name": "小芊",
+    "preview_url": "https://v2-kling.kechuangai.com/bs2/upload-ylab-stunt/muse/867844650139664460/AUDIO/20260504/e6212f4da1a230c8534de1185323af3d-f32c690d-fb5c-4bff-95aa-0ba162796e1f.quality.wav?x-kcdn-pid=112452",
+    "type": "clone"
+  },
+  {
+    "id": "880091802614272037",
+    "name": "智薇",
+    "preview_url": "https://v2-kling.kechuangai.com/bs2/upload-ylab-stunt/muse/867844650139664460/AUDIO/20260504/dc8628aaf1b4c784b45489ff1d45907c-f8229b28-2928-46dd-af5c-aca98882f64b.quality.wav?x-kcdn-pid=112452",
+    "type": "clone"
+  },
+  {
+    "id": "880091853004492887",
+    "name": "智萱",
+    "preview_url": "https://v2-kling.kechuangai.com/bs2/upload-ylab-stunt/muse/867844650139664460/AUDIO/20260504/d1e80e2d61e2d0e397c2d743c51d22c0-089ba870-a8d2-4639-9b4f-545d816e0fbf.quality.wav?x-kcdn-pid=112452",
+    "type": "clone"
+  },
+  {
+    "id": "880091901515948120",
+    "name": "智希",
+    "preview_url": "https://v2-kling.kechuangai.com/bs2/upload-ylab-stunt/muse/867844650139664460/AUDIO/20260504/90f82b1387dccdd5cf7052baaddce633-bd70f79a-393a-4e39-a5ca-a59669a79072.quality.wav?x-kcdn-pid=112452",
     "type": "clone"
   }
 ];
